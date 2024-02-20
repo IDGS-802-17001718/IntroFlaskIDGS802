6c352a6c47413bfca39955525f83b05177eec38e
MarioVargas-C <mario.vargasbuss@gmail.com>
2024-02-19 21:58:55 -0600

    LMVP Variables globales 19/02/2024

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response
+from flask_wtf.csrf import CSRFProtect
+from flask import flash
+from flask import g
+
 import forms
-app=Flask(__name__)
+app=Flask(__name__) 
+app.secret_key='Esta es la clave secreta'
+
+@app.errorhandler(400)
+def page_not_found(e):
+    return render_template('404.html'),404
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.before_request
+def before_request():
+    g.prueba='hola'
+    print('antes de ruta 1')
+
+@app.after_request
+def after_request(response):
+    print("Despues de ruta 3")
+    return response
+
 @app.route("/alumnos", methods=["GET", "POST"])
 def alumnos():
+    print('Dentro de ruta 2')
+    valor=g.prueba
+    print('el dato es: {}'.format(valor))
+    nom=''
     alum_form=forms.UserForm(request.form)
     if request.method=='POST' and alum_form.validate():
         nom=alum_form.nombre.data
@@ -15,12 +38,13 @@ def alumnos():
         amaterno=alum_form.amaterno.data
         edad=alum_form.edad.data
         email=alum_form.email.data
-        print('Nombre: {}'.format(nom))
+        mensaje='Bienvenido: {}'.format(nom)
+        flash(mensaje)
         print('Apaterno: {}'.format(apaterno))
         print('Amaterno: {}'.format(amaterno))
         print('Edad: {}'.format(edad))
         print('Correo: {}'.format(email))
-    return render_template("alumnos.html", form=alum_form)
+    return render_template("alumnos.html", form=alum_form, nom=nom)
     
 
 @app.route("/maestros")
